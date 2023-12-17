2fdca36acbc913cd424fafef67f75e3a55456857
Vyente David Ruffin <76492205+david-ruffin@users.noreply.github.com>
2023-12-16 21:46:22 -0800

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,12 @@
+# General
+variable "profile" {}
+variable "environment" {}
+variable "resource_group_name" {}
+variable "subscription_id" {}
+variable "client_id" {}
+variable "client_secret" {}
+variable "tenant_id" {}
+variable "location" {}
+variable "service_environment_name" {}
+variable "service_environment_name_resource_group_name" {}
+variable "custom_tags" {}
