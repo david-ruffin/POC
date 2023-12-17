fe50029a617738b5ef21191d963833c610b98c6f
Vyente David Ruffin <76492205+david-ruffin@users.noreply.github.com>
2023-12-16 21:45:58 -0800

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,27 @@
+# We strongly recommend using the required_providers block to set the
+# Azure Provider source and version being used
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.35.0"
+    }
+  }
+}
+
+# example terraform apply -var-file="dev.tfvars"
+resource "random_string" "random" {
+  length  = 4
+  special = false
+  upper   = false
+
+}
+
+
+provider "azurerm" {
+  subscription_id = var.subscription_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
+  features {}
+}
