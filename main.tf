b76ac8b094cdb1bab51b2844d547252739253218
Vyente David Ruffin <76492205+david-ruffin@users.noreply.github.com>
2023-12-17 20:29:59 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -34,7 +34,7 @@ resource "null_resource" "update_env" {
   depends_on = [azurerm_cognitive_account.example]
 
   provisioner "local-exec" {
-    command = "echo AI_SERVICE_ENDPOINT=${azurerm_cognitive_account.example.endpoint} > .env && echo AI_SERVICE_KEY=${azurerm_cognitive_account.example.primary_access_key} >> .env"
+    command = "echo AI_SERVICE_ENDPOINT=${azurerm_cognitive_account.example.endpoint} > ./Labfiles/01-analyze-images/Python/.env && echo AI_SERVICE_KEY=${azurerm_cognitive_account.example.primary_access_key} >> ./Labfiles/01-analyze-images/Python/.env"
   }
 }
 
