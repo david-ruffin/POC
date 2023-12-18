4648732ebcf706dc17248bc632007dbc51003c36
Vyente David Ruffin <76492205+david-ruffin@users.noreply.github.com>
2023-12-17 16:00:46 -0800

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,5 +1,5 @@
 # General
-variable "profile" {}
+# variable "profile" {}
 variable "environment" {}
 variable "resource_group_name" {}
 variable "subscription_id" {}
@@ -7,6 +7,6 @@ variable "client_id" {}
 variable "client_secret" {}
 variable "tenant_id" {}
 variable "location" {}
-variable "service_environment_name" {}
-variable "service_environment_name_resource_group_name" {}
+# variable "service_environment_name" {}
+# variable "service_environment_name_resource_group_name" {}
 variable "custom_tags" {}
